4b802db4f24029dc81bd3af521a3ce785b0dc985
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-11-14 10:43:51 -0500

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if (length <= 0):
+        print([])
+    elif (length == 1):
+        print([0])
+    else:
+        fib_list = [0, 1]
+        while (len(fib_list) < length):
+            new_num = fib_list[-1] + fib_list[-2]
+            fib_list.append(new_num)
+        print(fib_list)
+        
+
